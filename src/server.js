2f5931222c75c2f74566f82cd2acbc7ce6b59d05
wvxvw <olegsivokon@gmail.com>
2014-01-06 22:22:03 +0200

    Addes server files.

---
+++ src/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+        response.writeHead(200, { "Content-Type" : "text/plain" });
+        response.end("This will serve JSON one day\n");
+}).listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
